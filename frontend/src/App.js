3e3568bb917b21a4e478d2eb40dc06d9c0973995
SophieSchochet <87827703+SophieSchochet@users.noreply.github.com>
2024-04-14 12:04:09 -0500

    testing frontend connection

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
+import { getSchools } from '../../backend/functions';
 
 function App() {
+  const [schools, setSchools] = useState([]);
+  useEffect(() => {
+  if(schools === []){
+    getSchools().then(value => setSchools(value));
+  } }, []);
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {schools.map((school) => (
+          <p>
+            {school}
+          </p>
+        ))}
       </header>
     </div>
   );
